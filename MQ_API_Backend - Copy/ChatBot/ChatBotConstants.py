f624ba651e83dc25d2a8808434131bbc8566af34
Fergus2299 <fergusjcassidy@gmail.com>
2023-09-26 19:18:25 +0100

    Added the contents of the inner repo.

---
+++ MQ_API_Backend - Copy/ChatBot/ChatBotConstants.py
@@ -0,0 +1,3 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+# and rename this file to constants.py.
+APIKEY = "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"
