3f5e4e3092786e8c2970fb575017c670777a20cf
Fergus2299 <fergusjcassidy@gmail.com>
2023-09-26 23:18:25 +0100

    finished

--- MQ_API_BACKEND/JavaApp/BootJava.py
+++ MQ_API_BACKEND/JavaApp/BootJava.py
@@ -68,9 +68,9 @@ def start_spring_app_with_properties(queue_manager, channel, conn_name, user, pa
 
 if __name__ == "__main__":
     start_spring_app_with_properties(
-        queue_manager="QM1",
+        queue_manager="QM2",
         channel="DEV.ADMIN.SVRCONN",
-        conn_name="13.87.80.195(1414)",
+        conn_name="13.87.80.195(1415)",
         user="admin",
         password="passw0rd",
         listener_auto_startup="false"
